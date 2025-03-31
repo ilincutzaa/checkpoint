f9c9f0db61f7079a70ffb3bd894d8a025222dd5a
ilincamares <ilinca.mares@stud.ubbcluj.ro>
2025-03-31 19:30:11 +0300

    initial

---
+++ src/app/add/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css"
+
+export default function ItemForm(){
+    return (
+        <div className={styles.container}>
+            <h1>Add user</h1>
+        </div>
+
+    )
+}
