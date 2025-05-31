a295988ef3460a2d2ef3e8796c0b9691f55f9f63
ilincamares <ilinca.mares@stud.ubbcluj.ro>
2025-05-31 15:48:08 +0300

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,8 @@ export default function RootLayout({ children }) {
   return (
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          <div>{children}</div>
+            {children}
+            <Analytics />
         </body>
       </html>
   );
